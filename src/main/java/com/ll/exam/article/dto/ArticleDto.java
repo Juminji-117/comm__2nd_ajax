e67a25b22fdeb230d591aaa84ae89aef3ac92f33
Ju, Minji <alswl0w0@gmail.com>
2022-08-01 09:52:12 +0900

    toObj 에 제너릭 적용하여, 좀 더 편하게 쓸 수 있도록

--- src/main/java/com/ll/exam/article/dto/ArticleDto.java
+++ src/main/java/com/ll/exam/article/dto/ArticleDto.java
@@ -1,8 +1,8 @@
 package com.ll.exam.article.dto;
 
+import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
