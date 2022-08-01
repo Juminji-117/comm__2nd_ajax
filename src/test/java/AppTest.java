eae6773e57a523e0340ccecf84d47eec913569b6
Ju, Minji <alswl0w0@gmail.com>
2022-08-01 09:57:43 +0900

    복잡한 JSON 을 Map<String, ArticleDto> 형태로 변환

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -18,6 +18,7 @@ public class AppTest {
     }
 
     @Test
+        // ArticleDto => JS객체(단순)
     void ObjectMapper__articleDtoToJsonStr() {
         ArticleDto articleDto = new ArticleDto(1, "제목", "내용");
 
@@ -28,6 +29,7 @@ public class AppTest {
     }
 
     @Test
+        // List<ArticleDto> => JS배열
     void ObjectMapper__articleDtoListToJsonStr() {
         List<ArticleDto> articleDtos = new ArrayList<>();
         articleDtos.add(new ArticleDto(1, "제목1", "내용1"));
@@ -40,6 +42,7 @@ public class AppTest {
     }
 
     @Test
+        // Map<String, ArticleDto> => JS객체(복잡)
     void ObjectMapper__articleDtoMapToJsonStr() {
         Map<String, ArticleDto> articleDtoMap = new HashMap<>();
         articleDtoMap.put("가장오래된", new ArticleDto(1, "제목1", "내용1"));
@@ -51,6 +54,7 @@ public class AppTest {
     }
 
     @Test
+        // JS객체(단순) => ArticleDto
     void ObjectMapper__jsonStrToObj() {
         ArticleDto articleDtoOrigin = new ArticleDto(1, "제목", "내용");
         String jsonStr = Ut.json.toStr(articleDtoOrigin, "");
@@ -61,6 +65,7 @@ public class AppTest {
     }
 
     @Test
+        // JS배열 => List<ArticleDto>
     void ObjectMapper__jsonStrToArticleDtoList() {
         List<ArticleDto> articleDtos = new ArrayList<>();
         articleDtos.add(new ArticleDto(1, "제목1", "내용1"));
@@ -72,5 +77,19 @@ public class AppTest {
         }, null);
 
         assertThat(articleDtosFromJson).isEqualTo(articleDtos);
+    }
+
+    @Test
+        // JS객체(복잡) => Map<String, ArticleDto>
+    void ObjectMapper__jsonStrToArticleDtoMap() {
+        Map<String, ArticleDto> articleDtoMap = new HashMap<>();
+        articleDtoMap.put("가장오래된", new ArticleDto(1, "제목1", "내용1"));
+        articleDtoMap.put("최신", new ArticleDto(2, "제목2", "내용2"));
+        String jsonStr = Ut.json.toStr(articleDtoMap, "");
+
+        Map<String, ArticleDto> articleDtoMapFromJson = Ut.json.toMap(jsonStr, new TypeReference<>() {
+        }, null);
+
+        System.out.println(jsonStr);
     }
 }
